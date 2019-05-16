9fbb4b9cd2cd6783bf5adb98d69d9bcc063de1ef
rayacooper <47796461+rayacooper@users.noreply.github.com>
2019-05-15 20:23:31 -0600

    Basic Setup

---
+++ src/Components/Product/Product.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Product extends Component{
+    render(){
+        return(
+            <div>
+                PRODUCT
+            </div>
+        )
+    }
+}
+
+export default Product;
